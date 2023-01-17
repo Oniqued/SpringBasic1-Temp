786be918780c04a5e965f28531129d4d81ad1d97
Oniqued <zktl3575@gmail.com>
2023-01-17 19:42:05 +0900

    김영한 스프링 기본 - 회원 할인 정책 구현 및 테스트

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.member.Grade;
 import hello.core.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy{
@@ -8,7 +9,7 @@ public class FixDiscountPolicy implements DiscountPolicy{
 
     @Override
     public int discount(Member member, int price) {
-
-        return 0;
+        if(member.getGrade() == Grade.VIP) return discountFixAmount;
+        else return 0;
     }
 }
